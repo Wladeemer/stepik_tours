3fe58fbd0c978501f2f1825a78db64e205d4fa7e
Wladeemer <info@brigel.ru>
2022-02-16 23:02:43 +0300

    Start Project

---
+++ tours/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import render
+
+
+def main_view(request):
+    return render(request, "tours/index.html")
+
+
+def departure_view(request):
+    return render(request, "tours/departure.html")
+
+
+def tour_view(request, id):
+    return render(request, "tours/tour.html")
+
+
+def custom_handler404(request, exception):
+    # Call when Http404 raised
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    # Call when raised some python exception
+    return HttpResponseServerError('Ошибка сервера!')
